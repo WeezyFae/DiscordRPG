e47fe2933c7d731fcd244011f4663fe55f25e736
KingCosmic <KingCosmic2016@gmail.com>
2017-04-06 18:00:45 -0500

    beggining of quest bots
    added a quest bot base tho it still needs much much work

--- npcbases/normal.js
+++
@@ -1,5 +0,0 @@
-const npc = require('./npc');
-
-class normal extends npc {
-	
-}
---
+++ npcbases/quest.js
@@ -0,0 +1,8 @@
+const npc = require('./npc');
+
+class quest extends npc {
+	quest(name, description, reward, reward2, reward3) {
+
+		return;
+	}
+}
