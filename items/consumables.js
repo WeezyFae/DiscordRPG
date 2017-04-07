7f92ea908c72a9b43ca58d4d6223d1a437b44bdf
KingCosmic <KingCosmic2016@gmail.com>
2017-04-07 01:01:21 -0500

    stay away potion

--- items/consumables.js
+++ items/consumables.js
@@ -3,3 +3,9 @@ exports.healthPotion = {
 	name: "Health Potion",
 	heal: 40
 }
+
+exports.StayAwayPotion = {
+	name: "Stay Away Potion",
+	effect: "makes things not fight you",
+	//code to make chance go down here
+}
