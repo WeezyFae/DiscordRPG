91b11656599195ffa4761e4baf5230a0216860e4
E3nigma <enigmagamedevelopers@gmail.com>
2017-04-09 09:38:56 -0500

    Moves
    Added a new move to the list

---
+++ items/Moves(E).js
@@ -0,0 +1,22 @@
+moves: {
+  move1: {
+    name: "Slash",
+    dmg: 12
+  },
+  move2: {
+    name: "Spin Strike",
+    dmg: 15
+  },
+  move3: {
+    name: "Stab",
+    dmg:10
+  },
+  move4: {
+    name: "Thrust",
+    dmg: 8
+  },
+  move5: {
+    name: "OverHead Strike",
+    dmg: 15
+  }
+}
