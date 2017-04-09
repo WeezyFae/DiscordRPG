ea5d2cba8916d278ea7e580ab93ce9df8c063bc2
E3nigma <enigmagamedevelopers@gmail.com>
2017-04-09 09:39:08 -0500

    Consumables and armor

---
+++ items/Armor(E).js
@@ -0,0 +1,29 @@
+exports.starterHealm = {
+	name: "Starter Healm",
+	armor: 4,
+	type: "Healm"
+}
+
+exports.starterChest = {
+	name: "Starter Chest",
+	armor: 6,
+	type: "Chest"
+}
+
+exports.starterGauntlets = {
+	name: "Starter Gauntlets",
+	armor: 3,
+	type: "Gauntlets"
+}
+
+exports.starterBoots = {
+	name: "Starter Boots",
+	armor: 3,
+	type: "Boots"
+}
+
+exports.starterGreaves = {
+  name: "Starter Greaves"
+  armor: 4,
+  type: "Greaves"
+}
