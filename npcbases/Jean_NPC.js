e759063ba5d9ed8ade5bc5600503080498c8da34
Timbourdon <tim@boudev.com>
2017-04-10 21:13:54 +0200

    Fixing embed Jean-NPC

--- npcbases/Jean_NPC.js
+++ npcbases/Jean_NPC.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'My bot token';
+const token = 'Mjk3MDA3MjIxMjM2MzAxODI1.C76hFA.s9twJqWFlW7n3GZBu82_Tps-cEc';
 
 function commandIs(str, msg){
     return msg.content.toLowerCase().startsWith("!" + str);
@@ -27,11 +27,11 @@ client.on('ready', () => {
        if(commandIs('hello_jean', message)) {
          const helloJean = new Discord.RichEmbed()
          .setTitle("Hello")
-         .setAuthor("Jean", msg.author.avatarURL)
+         .setAuthor("Jean", message.author.avatarURL)
 
          .setColor(0x00AE86)
-         .addField("Hello there," message.author.username)
-         msg.channel.sendEmbed(helloJean);
+         .addField("Hello there, ",  message.author.username)
+         message.channel.sendEmbed(helloJean);
           }
           });
 
@@ -42,13 +42,13 @@ client.on('ready', () => {
       if(commandIs('lore1_jean', message)) {
         const lore1Jean = new Discord.RichEmbed()
         .setTitle("Lore")
-        .setAuthor("Jean", msg.author.avatarURL)
+        .setAuthor("Jean", message.author.avatarURL)
 
         .setColor(0x00AE86)
         .addField("Welcome young adventurer. Before you head out, you should know something. Would you like to listen to my story?")
         .addField("listen_jean")
         .addField("leave_jean")
-        msg.channel.sendEmbed(lore1Jean);
+        message.channel.sendEmbed(lore1Jean);
       }
     });
 
@@ -58,12 +58,12 @@ client.on('ready', () => {
         if(commandIs('listen_jean', message)) {
           const listJean = new Discord.RichEmbed()
           .setTitle("Listen")
-          .setAuthor("Jean", msg.author.avatarURL)
+          .setAuthor("Jean", message.author.avatarURL)
 
           .setColor(0x00AE86)
           .addField("Thank you for staying and listening to an old man like me.")
           .addField("There are going to be many troubles along your journey. If you believe and have hope, you can survive these horrors, but do not let them taint you.")
-          msg.channel.sendEmbed(listJean);
+          message.channel.sendEmbed(listJean);
         }
       });
 
@@ -73,11 +73,11 @@ client.on('ready', () => {
         if(commandIs('leave_jean', message)) {
           const leafJean = new Discord.RichEmbed()
           .setTitle("Leave")
-          .setAuthor("Jean", msg.author.avatarURL)
+          .setAuthor("Jean", message.author.avatarURL)
 
           .setColor(0x00AE86)
           .addField("Good-bye then young adventurer. Come back to us safely.")
-          msg.channel.sendEmbed(leafJean);
+          message.channel.sendEmbed(leafJean);
         }
       });
 
@@ -90,11 +90,11 @@ client.on('ready', () => {
         var ra = array[Math.floor(Math.random()*array.length)];
         const sayJean = new Discord.RichEmbed()
         .setTitle("Say")
-        .setAuthor("Jean", msg.author.avatarURL)
+        .setAuthor("Jean", message.author.avatarURL)
 
         .setColor(0x00AE86)
         .addField(ra)
-        msg.channel.sendEmbed(sayJean);
+        message.channel.sendEmbed(sayJean);
          }
     });
 
